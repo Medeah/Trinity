afcb80f19a0c455ac89b4b225f50f3158facf076
Frederik Andersen <fhan13@student.aau.dk>
2015-03-13 10:21:59 +0100

    new main

--- src/main/java/Trinity.java
+++ src/main/java/Trinity.java
@@ -1,22 +1,41 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
 public class Trinity {
 
     public static void main(String[] args) throws Exception {
-        String inputFile = null;
-        if (args.length > 0) inputFile = args[0];
-        InputStream is = System.in;
-        if (inputFile != null) is = new FileInputStream(inputFile);
+        if (args.length != 1) {
+            showUsage();
+        }
+
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(args[0]));
+            String is =  new String(encoded, Charset.defaultCharset());
+            String out = compile(is);
+            System.out.println(out);
+        } catch (NoSuchFileException test) {
+            System.out.println("file not found: " + test.getMessage());
+            showUsage();
+        }
+    }
+
+    private static void showUsage() {
+        System.out.println("Usage: Trinity <source file>");
+        System.exit(1);
+    }
+
+    private static String compile(String is) {
         ANTLRInputStream input = new ANTLRInputStream(is);
         TrinityLexer lexer = new TrinityLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         TrinityParser parser = new TrinityParser(tokens);
         ParseTree tree = parser.prog();
-        System.out.println(tree.toStringTree(parser));
+        return tree.toStringTree(parser);
     }
-
 }

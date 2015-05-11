3776fec02b9285ab41c134cb8d4405ec8b389739
Kasper Helsted <khelst13@student.aau.dk>
2015-05-11 12:26:25 +0200

    bit cleanup

--- src/main/java/trinity/visitors/ReachabilityVisitor.java
+++ src/main/java/trinity/visitors/ReachabilityVisitor.java
@@ -37,29 +37,13 @@ public class ReachabilityVisitor extends TrinityBaseVisitor<Boolean> implements 
     @Override
     public Boolean visitBlock(TrinityParser.BlockContext ctx){
         int stmtSize = ctx.stmt().size();
-        boolean isReturnValid = (ctx.returnStmt() != null) ? ctx.returnStmt().accept(this) : false;
+        boolean isReturnValid = (ctx.returnStmt() != null) ? ctx.returnStmt().accept(this) : false, returnFound = false;
 
-        //checks if a function does not contain any statements or returns.
-        //ignores blocks who is not directly in the function base.
-        if(ctx.parent instanceof TrinityParser.FunctionDeclContext && stmtSize == 0 && !isReturnValid){
-            return false;
+        for (int i = 0; i < stmtSize; i++) {
+            returnFound = ctx.stmt(i).accept(this);
         }
 
-        if(stmtSize == 0 && !isReturnValid) {
-            return false;
-        }else{
-            boolean returnFound = false;
-
-            for (int i = 0; i < stmtSize; i++) {
-                returnFound = ctx.stmt(i).accept(this);
-            }
-
-            if(!(returnFound || isReturnValid)){
-                return false;
-            }
-        }
-
-        return true;
+        return (returnFound || isReturnValid);
     }
 
     @Override
@@ -74,10 +58,15 @@ public class ReachabilityVisitor extends TrinityBaseVisitor<Boolean> implements 
 
     @Override
     public Boolean visitIfStatement(TrinityParser.IfStatementContext ctx){
-        boolean contentFound = (ctx.block().size() != 0) ? true : false;
+        int blocks = ctx.block().size(), expr = ctx.expr().size();
+        boolean contentFound = (blocks != 0) ? true : false;
 
-        for (int i = 0; i < ctx.block().size(); i++){
-            if(!ctx.block(i).accept(this)){ contentFound = false; }
+        for (int i = 0; i < ((blocks != expr) ? expr : blocks); i++){
+            contentFound = ctx.block(i).accept(this);
+        }
+
+        if(ctx.expr().size() != ctx.block().size() && contentFound){
+            return ctx.block(blocks-1).accept(this);
         }
 
         return contentFound;
@@ -93,16 +82,11 @@ public class ReachabilityVisitor extends TrinityBaseVisitor<Boolean> implements 
     @Override
     public Boolean visitBlockStatement(TrinityParser.BlockStatementContext ctx) {
         // Simply return whatever the block returns, but do accept empty blocks.
-        if(ctx.block().getText().isEmpty()){
-            return false;
-        }else if (ctx.block().returnStmt() != null){
-            return ctx.block().returnStmt().accept(this);
-        }
         return ctx.block().accept(this);
     }
 
     @Override
     public Boolean visitForLoop(TrinityParser.ForLoopContext ctx) {
-        return true;
+        return ctx.block().accept(this);
     }
 }

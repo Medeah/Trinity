1429c163bd48b763a2d8e12669b5d466f37c64d2
mathiasvr <mathiasvr@gmail.com>
2015-05-18 13:06:48 +0200

    windows mp

--- src/test/java/trinity/tests/CodeGenerationVisitorTest.java
+++ src/test/java/trinity/tests/CodeGenerationVisitorTest.java
@@ -21,24 +21,26 @@ public class CodeGenerationVisitorTest {
         pw.println(out);
         pw.flush();
 
-        Process process = new ProcessBuilder("gcc", "test.c", "-lm").start();
+        Process process = new ProcessBuilder("gcc", "test.c", "-lm", "-o", "a.bin").start();
         if (process.waitFor() != 0) {
             throw new Exception("Error compiling c code");
         }
 
-        Process prog = new ProcessBuilder("./a.out").start();
+        Process prog = new ProcessBuilder("./a.bin").start();
         if (prog.waitFor() != 0) {
             throw new Exception("program exited with non zero exit code");
         }
 
         InputStreamReader inr = new InputStreamReader(prog.getInputStream());
-        return CharStreams.toString(inr);
+
+        // Remove CR line-endings for Windows
+        return CharStreams.toString(inr).replace("\r", "");
     }
 
     @AfterClass
     public static void removeFiles() throws Exception {
         delete(FileSystems.getDefault().getPath("test.c"));
-        delete(FileSystems.getDefault().getPath("a.out"));
+        delete(FileSystems.getDefault().getPath("a.bin"));
     }
 
     @Test

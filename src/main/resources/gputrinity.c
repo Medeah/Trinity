1e0d85c4100b0597920250ede485008df61129e5
mathiasvr <mathiasvr@gmail.com>
2015-05-25 11:48:30 +0200

    Test det her @GideonBlegmand

--- src/main/resources/gputrinity.c
+++ src/main/resources/gputrinity.c
@@ -13,44 +13,44 @@ float* mmmult(const float *A, size_t rowsA, size_t colsA, const float *B, size_t
 	// Allocate memory on Device
 	error = cudaMalloc(&d_A, rowsA * colsA * sizeof(float));
 	if (error != cudaSuccess) {
-		printf("Memory was not allocated for matrix A");
+		fprintf(stderr, "Memory was not allocated for matrix A");
 		exit(EXIT_FAILURE);
 	}
 
 	error = cudaMalloc(&d_B, rowsB * colsB * sizeof(float));
 	if (error != cudaSuccess) {
-		printf("Memory was not allocated for matrix B");
+		fprintf(stderr, "Memory was not allocated for matrix B");
 		exit(EXIT_FAILURE);
 	}
 
 	error = cudaMalloc(&d_temp, rowsC * colsC * sizeof(float));
 	if (error != cudaSuccess) {
-		printf("Memory was not allocated for matrix C");
+		fprintf(stderr, "Memory was not allocated for matrix C");
 		exit(EXIT_FAILURE);
 	}
 
-	//Copy h_A and h_B to the device
+	// Copy h_A and h_B to the device
 	error = cudaMemcpy(d_A, A, rowsA * colsA * sizeof(float), cudaMemcpyHostToDevice);
 	if (error != cudaSuccess) {
-		printf("Copying matrice h_A HtoD failed");
+		fprintf(stderr, Copying matrice h_A HtoD failed");
 		exit(EXIT_FAILURE);
 	}
 
 	error = cudaMemcpy(d_B, B, rowsB * colsB * sizeof(float), cudaMemcpyHostToDevice);
 	if (error != cudaSuccess){
-		printf("Copying matrice h_B HtoD failed");
+		fprintf(stderr, Copying matrice h_B HtoD failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// Multiplication on the device
 	gpu_blas_mmul(d_A, d_B, d_temp, rowsA, colsB, colsA);
 
-	//Copy result back to the host
-	float *h_temp = (float*)calloc(rowsA * colsB, sizeof(float));
+	// Copy result back to the host
+	float *h_temp = (float*)malloc(rowsA * colsB * sizeof(float));
 
 	error = cudaMemcpy(h_temp, d_temp, rowsC * colsC * sizeof(float), cudaMemcpyDeviceToHost);
 	if (error != cudaSuccess){
-		printf("Copying matrix d_C DtoH failed iteration");
+		fprintf(stderr, "Copying matrix d_C DtoH failed iteration");
 		exit(EXIT_FAILURE);
 	}
 
@@ -61,6 +61,8 @@ float* mmmult(const float *A, size_t rowsA, size_t colsA, const float *B, size_t
 		int j = n % colsC;
 		C[n] = h_temp[rowsC * j + i];
 	}
+
+	free(h_temp);
 
 	cudaFree(d_A);
 	cudaFree(d_B);
